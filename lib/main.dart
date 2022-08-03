c383fbd059249dce447fb4a545d21ca255dd6d5c
channthoeun <channthoeun@gmail.com>
2022-08-03 22:16:24 +0700

    Added header search & icon, body with text & 3 buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,70 @@ void main() {
 class MyFirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('My First Flutter app from scratch...'),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          title: Text(
+             'Test Your Knowledge...',
+              style: TextStyle(
+                fontSize: 25.00,
+                fontStyle: FontStyle.normal
+              ),
+            ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null
+          ),
+        ],
+        ),
+      body: Column(
+        children: [
+          Text(
+            'You need to answer a few question',
+            style: TextStyle(
+                fontSize: 25,
+              ),
+          ),
+          RaisedButton(
+            child: Text(
+              'You have chosen answer 1',
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text(
+              'You have chosen answer 2',
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text(
+              'You have chosen answer 3',
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            onPressed: null,
+          ),
+        ],
+      ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Add',
+          child: Icon(Icons.add),
+          onPressed: null,
+        ),
+      ),
     );
   }
 }
