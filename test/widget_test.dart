d857e7cd5baf30f19b173528d7ea4a70599f002c
channthoeun <channthoeun@gmail.com>
2022-08-04 22:49:56 +0700

    Updated test of MyFirstApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:my_first_flutter_app/my_first_app.dart';
 
 import 'package:my_first_flutter_app/main.dart';
 
