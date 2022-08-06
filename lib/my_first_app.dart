acef855e8065ea167156785a7181fc0a649c53be
channthoeun <channthoeun@gmail.com>
2022-08-06 10:16:37 +0700

    Used MyContainerWidget in MyFirstApp

--- lib/my_first_app.dart
+++ lib/my_first_app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter_app/my_container.dart';
 import 'package:my_first_flutter_app/my_stateless_scaffold.dart';
 
 class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: MyStatelessScaffoldWidget(),
+      home: MyContainerWidget(),
     );
   }
 }
