fbf83278fed60e13aba467d636344460e2c6a55f
channthoeun <channthoeun@gmail.com>
2022-08-06 13:52:13 +0700

    Added snackbar

---
+++ lib/snackbar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+final SnackBar snackBarOne = const SnackBar(
+    content: Text(
+      'Alert has been pressed',
+      style: TextStyle(fontSize: 30),
+    )
+);
+
+final SnackBar snackBarTwo = const SnackBar(
+  content: Text(
+    'Search has been pressed',
+    style: TextStyle(fontSize: 30),
+  )
+);
+
+final SnackBar snackBarThree = const SnackBar(
+    content: Text(
+      'Navigation has been pressed',
+      style: TextStyle(fontSize: 30),
+    )
+);
+
+final SnackBar snackBarFour = const SnackBar(
+    content: Text(
+      'You have chosen one. You are at safe zone!',
+      style: TextStyle(fontSize: 30),
+    )
+);
+
+final SnackBar snackBarFive= const SnackBar(
+    content: Text(
+      'You have chosen two. You need to be careful!',
+      style: TextStyle(fontSize: 30),
+    )
+);
+
+final SnackBar snackBarSix = const SnackBar(
+    content: Text(
+      'You have chosen three. You are at danger zone!',
+      style: TextStyle(fontSize: 30),
+    )
+);
+
