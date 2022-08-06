ca0fa7778d2445480e1d8f7d2ca6cfd3cbc931c8
channthoeun <channthoeun@gmail.com>
2022-08-06 14:58:41 +0700

    Used MyNewLayout in MyFirstApp

--- lib/my_first_app.dart
+++ lib/my_first_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_flutter_app/layout_first_example.dart';
+import 'package:my_first_flutter_app/my_new_layout.dart';
 
 class MyFirstApp extends StatelessWidget {
   const MyFirstApp({Key? key}) : super(key: key);
@@ -7,7 +8,7 @@ class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: MyStatelessLayoutWidget(),
+      home: MyNewLayout(),
     );
   }
 }
